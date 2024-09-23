88130f49ce6fa97eab4e99ea547d53b72c0c6234
Karu <44472731+ctuais@users.noreply.github.com>
2024-09-22 20:07:13 -0400

    Update primo-std.cpp
    primo-std+you

--- primo-std.cpp
+++ primo-std.cpp
@@ -1,34 +1,28 @@
 /*
-	N�meros primos en C++
+	Números primos en C++
 	inicia con 2 y finaliza con $limite.
 */
 #include <iostream> //std::cout, std::endl
 #include <windows.h> //b1 GetStdHandle, SetConsoleTextAttribute, SetConsoleTitle
 #include <iomanip> //b2 //std::setw(3) << std::setfill('0')
 
-#define $limite 100 //l�mite de b�squeda
+#define $limite 100 //límite de búsqueda
 
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); //b1
 
 bool primo(int numero)
 {
-    int x=2;
-    double numeroDividido=(double)numero/((double)x);
-    bool retorno=true;
-    if (numero == 0 || numero == 1)
-        retorno=false;
-    else
-    {
-        while ((x <= (numeroDividido)) && retorno)
-        {
-            if (numero % x == 0)
-                retorno=false; //no es primo
-            x++;
-            numeroDividido=(double)numero/((double)x);
-        }
-    }
-    return retorno;
-}
+    if (numero <= 1)
+        return false; // Números menores o iguales a 1 no son primos
+    if (numero <= 3)
+        return true;  // 2 y 3 son primos
+    if (numero % 2 == 0 || numero % 3 == 0)
+        return false; // Eliminar múltiplos de 2 y 3
+    for (int i = 5; (i*i) <= numero; i += 6)
+        if (numero % i == 0 || numero % (i + 2) == 0)
+            return false; // Verificar divisores
+    return true; // Si no se encontró divisor, es primo
+};
 
 int main(void)
 {
@@ -64,4 +58,3 @@ int main(void)
     (getchar());
     return 0;
 }
-
